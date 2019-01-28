954875d2d89114e57cf2c339900c4b65fa60c691
zhangyanl02 <zhangyanl02@163.com>
2019-01-28 14:37:35 +0800

    Delete rsparm_mod.F90

--- rsparm_mod.F90
+++
@@ -1,6 +0,0 @@
-module rsparm_mod
- use shr_kind_mod, only: r8 => shr_kind_r8
- implicit none
- real(r8)::RESMA,RESMB,RESMC,RESTKA,RESTKB
- DATA RESMA/-53.72/ RESMB/1.32/ RESMC/0.0/ RESTKA/0.007/ RESTKB/4.0/
-end module rsparm_mod
